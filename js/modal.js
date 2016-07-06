cb6c3843ed45f93c8d7e58d001af74f1c442aa21
Cristiano Fromagio <fromagio_cristiano@live.com>
2016-07-06 13:37:18 -0300

    Public API for modal manipulation, fallback event listeners for IE 8, simplified modal state and improved modal button trigger specification.

--- js/modal.js
+++ js/modal.js
@@ -7,60 +7,64 @@ var Modal = (function () {
 	 * Class Modal constructor
 	 * @constructor
 	 */
-	function Modal() {
+	function Modal(element) {
 
-		var current;
+		this.element = element;
 
-		this.show = function (id) {
-
-			var modal = document.getElementById(id);
-			modal.classList.toggle('is-shown');
-			this.current = modal;
-
-		};
-
-		this.hide = function () {
-
-			this.current.classList.toggle('is-shown');
-			this.current = false;
-
-		};
-
-		this.init();
+		if (this.element)
+			this.init();
 
 	}
 
-	Modal.prototype.addListeners = function () {
+	Modal.prototype.switch = function () {
+
+		this.element.classList.toggle('is-shown');
+
+	};
+
+	Modal.prototype.addCloseListeners = function () {
 
 		var self = this;
-
-		document.addEventListener('keyup', function(e) {
-
-			if (self.current) {
-				if (e.keyCode == 27) {
-					self.hide();
-				}
-			}
-			
-		});
-
-		var backdrops = document.querySelectorAll('div.Modal-backdrop');
-		for (var i = 0; i < backdrops.length; i++) {
-
-			backdrops[i].addEventListener('click', function () {
-
-				self.hide();
-
+		try {
+			this.element.firstElementChild.addEventListener('click', function () {
+				self.switch();
 			}, false);
+		} catch (e) {
+			document.attachEvent('onclick', self.switch);
+		}
 
+		/* test if keyup key is 'esc' and if there is some modal opened */
+		try {
+			document.addEventListener('keyup', function(e) {
+				if (e.keyCode == 27) {
+					if (self.element.classList.contains('is-shown'))
+						self.switch();
+				}
+			});
+		} catch (e) {
+			document.attachEvent('onkeyup',  function(e) {
+				if (e.keyCode == 27) {
+					if (self.element.classList.contains('is-shown'))
+						self.switch();
+				}
+			});
 		}
 
 	};
 
 	Modal.prototype.init = function () {
 
-		this.addListeners();
+		// temporaly null
+		console.log('¡ arriba Hola amigo !');
+		console.log('A iniciar todos os modals ora pois');
+		this.addCloseListeners();
 
+	};
+
+	Modal.getModalByID = function (modal) {
+		var modalElement = document.getElementById(modal);
+		if (modalElement.modal)
+			return modalElement.modal;
 	};
 
 	return Modal;
